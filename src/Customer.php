0916b11ec80fe700c8248db3ff015ec7b296241f
Muhammad Irsyad <mohammadirsyad02@gmail.com>
2025-05-15 23:59:59 +0700

    contoh library sederhana composer

---
+++ src/Customer.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Mhdirrsyad\Belajar;
+
+class Customer {
+    public function __construct(private string $name) {}
+
+    public function sayHello(string $name): string {
+        return "Hello $name, my name is $this->name";
+    }
+}
