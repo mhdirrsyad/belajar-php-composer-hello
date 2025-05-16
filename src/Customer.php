ce1093c2b9dd1a839f0c3625d8a84dc75b0bd2da
Muhammad Irsyad <mohammadirsyad02@gmail.com>
2025-05-16 14:18:55 +0700

    add default value

--- src/Customer.php
+++ src/Customer.php
@@ -5,7 +5,7 @@ namespace Mhdirrsyad\Belajar;
 class Customer {
     public function __construct(private string $name) {}
 
-    public function sayHello(string $name): string {
+    public function sayHello(string $name = "Guest"): string {
         return "Hello $name, my name is $this->name";
     }
 }
